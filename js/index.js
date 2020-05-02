1827ee8f6b8d630d5642d298c3bc027a86ef0121
tsutomu3011 <t.08032323556@gmail.com>
2020-05-02 17:53:36 +0900

    Create a transition of img using js.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 $(function(){
 
-  $('.info__title').click(function() {
-    console.log('test');
+
+
+
+  // 努
+  $('#1').click(function() {
+    $('.info__left__content').removeClass('active');
+    $('#1_content').addClass('active');
   });
+
+  $('#2').click(function() {
+    $('.info__left__content').removeClass('active');
+    $('#2_content').addClass('active');
+  });
+
+  $('#3').click(function() {
+    $('.info__left__content').removeClass('active');
+    $('#3_content').addClass('active');
+  });
+
 });
 
 

b3d37a9a4e6738a0ae7b2f4327b9a703c345e6c0
shun-pro <@shun.80367246@gmail.com>
2020-05-13 20:57:59 +0900

    tomerge

--- js/index.js
+++ js/index.js
@@ -1,21 +1,38 @@
-$(function(){
+// 駿
+var images = [
+  "url(https://www.yakiniku.jp/sakai/wp-content/uploads/2018/07/1450_1200_topimage_02.png)",
+  "url(https://www.yakiniku.jp/sakai/wp-content/uploads/2018/07/1450_1200_topimage_01.png)",
+  "url(https://www.yakiniku.jp/sakai/wp-content/uploads/2018/07/1450_1200_topimage_02.png)"
+]; // ランダム表示させたい画像のパス
 
-  $('.info__title').click(function() {
-    console.log('test');
+$(function() {
+  var backgroundRandom = function() {
+    var number = Math.floor(Math.random() * images.length); // 0~3の数値を算出
+    var selectedImg = images[number]; // 算出された数値を元に、1行目の配列から取り出す
+    $(".main_img").css("background-image", selectedImg); // cssにランダムに選ばれた画像を背景設定する
+  };
+  setInterval(backgroundRandom, 3000); // 1000msごとにランダム切り替えを繰り返す
+});
+// $(function() {
+//   // インナークラスの中のli要素にホバーしたら
+//   $(".inner li").hover(function() {
+//     $(this)
+//       .next(".inner")
+//       .slideToggle();
+
+//     $(".inner li")
+//       .not($(this))
+//       .next("inner")
+//       .slideUp();
+//   });
+// });
+// ここまで
+$(function() {
+  $(".info__title").click(function() {
+    console.log("test");
   });
 });
-
-
-
-
-
-
-
-
-
 
 // import "./shun"
 // import "./tsutomu"
 // import "./ucchi"
-
-
